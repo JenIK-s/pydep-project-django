1645d7466b7048a39d4e4f63818351ead08fbd77
jeniks <fertuv68@gmail.com>
2023-08-29 01:11:37 +0300

    1. Add page Courses 2. Add html template and add css 3. Add test logo

--- pydep/lesson/views.py
+++ pydep/lesson/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'lesson/index.html')
+
+
+def courses_list(request):
+    return render(request, 'lesson/courses_list.html')
