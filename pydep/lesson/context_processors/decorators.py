910caab9d9a4139417f0e1e25d40657feff9cc37
jeniks <fertuv68@gmail.com>
2023-09-12 12:36:53 +0300

    Добавил в профайл фильтрацию преподавателя и студента

---
+++ pydep/lesson/context_processors/decorators.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def course_required(func):
+    def check_course_in_user(*args, **kwargs):
+        try:
+            learn_course = args[0].user.courses_learn.get(name=kwargs.get('course_name'))
+            result = func(*args, **kwargs)
+            return result
+        except:
+            return render(args[0], 'lesson/permission_denied.html')
+    return check_course_in_user
