837dd880d1186a7bae4c9ca05de5307de05b6b6e
DmitryGanuskus <dima.zatolokin.99@mail.ru>
2023-09-12 18:00:34 +0400

    Добавил context processor для вывода фильтров курсов в header.html и сделал view для  отображения курсов по этим фильтрам

--- pydep/lesson/context_processors/filters.py
+++ pydep/lesson/context_processors/filters.py
@@ -1,1 +1,9 @@
-# def search():
+from ..models import Course
+
+
+def prog_langs(request):
+    return {
+        'prog_langs': set(
+            course.programming_language for course in Course.objects.all()
+        )
+    }
--- pydep/pydep/settings.py
+++ pydep/pydep/settings.py
@@ -46,6 +46,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'lesson.context_processors.filters.prog_langs',
             ],
         },
     },
@@ -131,7 +133,8 @@ customColorPalette = [
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+                    'bulletedList', 'numberedList', 'blockQuote',
+                    'imageUpload', ],
 
     },
     'extends': {
@@ -142,14 +145,19 @@ CKEDITOR_5_CONFIGS = {
             '|',
             'blockQuote',
         ],
-        'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough',
-                    'code', 'subscript', 'superscript', 'highlight', '|', 'codeBlock', 'sourceEditing', 'insertImage',
-                    'bulletedList', 'numberedList', 'todoList', '|', 'blockQuote', 'imageUpload', '|',
-                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
+        'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic',
+                    'link', 'underline', 'strikethrough',
+                    'code', 'subscript', 'superscript', 'highlight', '|',
+                    'codeBlock', 'sourceEditing', 'insertImage',
+                    'bulletedList', 'numberedList', 'todoList', '|',
+                    'blockQuote', 'imageUpload', '|',
+                    'fontSize', 'fontFamily', 'fontColor',
+                    'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
                     'insertTable', ],
         'image': {
             'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
-                        'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side', '|'],
+                        'imageStyle:alignRight', 'imageStyle:alignCenter',
+                        'imageStyle:side', '|'],
             'styles': [
                 'full',
                 'side',
@@ -173,10 +181,14 @@ CKEDITOR_5_CONFIGS = {
         },
         'heading': {
             'options': [
-                {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
-                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
-                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
-                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'}
+                {'model': 'paragraph', 'title': 'Paragraph',
+                 'class': 'ck-heading_paragraph'},
+                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1',
+                 'class': 'ck-heading_heading1'},
+                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2',
+                 'class': 'ck-heading_heading2'},
+                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3',
+                 'class': 'ck-heading_heading3'}
             ]
         }
     },
