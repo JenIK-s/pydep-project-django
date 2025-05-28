09bdff6f22222c45868f3a3e378bfcbc978257d6
jeniks <fertuv68@gmail.com>
2025-05-28 14:27:06 +0300

    - Добавлены сериализаторы для создания и чтения курса (модулей и уроков) и пользователя - Добавлены вьюсеты для них - Прописаны маршруты

--- pydep/api/views.py
+++ pydep/api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import (
+    UsersSerializer, CourseGetSerializer, CoursePostSerializer,
+    ModuleGetSerializer, ModulePostSerializer, LessonGetSerializer,
+    LessonPostSerializer,
+)
+
+from users.models import CustomUser
+from lesson.models import Lesson, Module, Course
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UsersSerializer
+    queryset = CustomUser.objects.all()
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return CoursePostSerializer
+        return CourseGetSerializer
+
+
+class ModuleViewSet(ModelViewSet):
+    queryset = Module.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return ModulePostSerializer
+        return ModuleGetSerializer
+
+
+class LessonViewSet(ModelViewSet):
+    queryset = Lesson.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return LessonPostSerializer
+        return LessonGetSerializer
