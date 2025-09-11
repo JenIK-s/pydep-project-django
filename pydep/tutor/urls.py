927a0bcc7648eb5876c42103e9c8e1edd737c659
JenIK-s <fertuv68@gmail.com>
2025-09-11 19:20:25 +0300

    - Подготовлено окружение для тестов pytest - Добавлен профиль в tutor - Исправлен цвет в projectbox.html на красный

--- pydep/tutor/urls.py
+++ pydep/tutor/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import my_tutor
 from .views import projectbox
+from .views import profile
 
 app_name = 'tutor'
 
 urlpatterns = [
     path('', my_tutor, name='my_tutor'),
     path('projectbox/', projectbox, name='projectbox'),
+    path('profile/', profile, name='profile'),
 ]
--- pydep/tutor/views.py
+++ pydep/tutor/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import FilesProject
@@ -10,3 +11,33 @@ def my_tutor(request):
 def projectbox(request):
     files = FilesProject.objects.all()
     return render(request, "tutor/projectbox.html", {"files": files})
+
+
+@login_required
+def profile(request):
+    """
+    Профиль пользователя. Отображение роли, проходимый и преподаваемых курсов
+    """
+    user = request.user
+    result = ''
+    if user.is_superuser:
+        result += 'Модератор '
+    if user.is_teacher:
+        result += 'Преподаватель '
+    if user.is_student:
+        result += 'Студент '
+    is_teacher = user.is_teacher
+    is_student = user.is_student
+    result = result.strip()
+    result = result.replace(' ', ' & ')
+
+
+    context = {
+        'result': result,
+        'user': user,
+        'is_teacher': is_teacher,
+        'is_student': is_student,
+        'is_tutor_student': user.is_tutor_student,
+    }
+
+    return render(request, 'tutor/profile.html', context)
